ab0728d3d869e8d9e0fcc6408db87e2895947516
Parth Iyer <parth.iyer@gmail.com>
2026-02-08 01:37:45 -0500

    Updated BasePlatformHandler to match new spec for Android folder stuff support

--- src/main/java/basewindow/BasePlatformHandler.java
+++ src/main/java/basewindow/BasePlatformHandler.java
@@ -1,8 +1,10 @@
 package basewindow;
 
-public abstract class BasePlatformHandler
-{
+public abstract class BasePlatformHandler {
+
     public abstract void quit();
 
     public abstract void openLink(String url);
+
+    public abstract void openFolder(String path);
 }
